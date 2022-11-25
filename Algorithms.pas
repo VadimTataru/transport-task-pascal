f1f8bbbf5e41c14136227bda22afb4f734c88ffb
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-11-26 02:20:44 +0300

    feat: find potentials

--- Algorithms.pas
+++ Algorithms.pas
@@ -4,21 +4,26 @@ interface
 
 type TransportTaskSolver = record
   rateMatrix: array[,] of real;
+  flagContent: array[,] of boolean;
   storagesVector: array of real;
   shopsVector: array of real;
   
   public constructor Create(
     rateMatrix: array[,] of real;
+    flagContent: array[,] of boolean;
     storagesVector: array of real;
     shopsVector: array of real
   );
   begin
     self.rateMatrix := rateMatrix;
+    self.flagContent := flagContent;
     self.storagesVector := storagesVector;
     self.shopsVector := shopsVector;
   end;
   
-  procedure Print;  
+  procedure print;
+  procedure findPotentials(storegesPotentials, shopsPotentials: array of real; matrix: array[,] of real);
+  
   function minTariffMethod(): array[,] of real;
   function calculateFunction(matrix: array[,] of real): real;
   
@@ -28,13 +33,19 @@ end;
 function findIndecesOfMinRate(matrix: array[,] of real): (integer, integer);
 function getIndexOfMinInRow(matrix: array[,] of real; row: integer; member: array of real; isRowVertical: boolean): integer;
 function isMemberEmpty(member: array of real): boolean;
+function indexOfNaN(vector: array of real): integer;
 
 //procedires
 procedure printMatrix(matrix: array[,] of real);
+procedure printVector(vector: array of real);
+procedure fillVectorWithBool(vector: array of boolean; fillBy: boolean);
+procedure fillVectorWithNaN(vector: array of real);
 
 implementation
 
-procedure TransportTaskSolver.Print;
+//Self func and procedure. Start.
+
+procedure TransportTaskSolver.print;
 var charSpace: integer := 7;
 begin
   Write('':charSpace, '│');
@@ -44,19 +55,9 @@ begin
   Writeln('————————————————————————————————————————————————————');
 end;
 
-function findIndecesOfMinRate(matrix: array[,] of real): (integer, integer);
-begin
-  Result:= (0,0);  
-  for var i:= 0 to matrix.GetLength(0) - 1 do
-    for var j:= 0 to matrix.GetLength(1) - 1 do
-      if(matrix[i,j] < matrix[Result[0], Result[1]]) then
-        Result:= (i, j);  
-end;
-
 function TransportTaskSolver.minTariffMethod(): array[,] of real;
 var
   k, s: integer;
-  flag:= true;
 begin
   Result:= new real[self.rateMatrix.GetLength(0), self.rateMatrix.GetLength(1)];
   //Шаг 1
@@ -65,7 +66,8 @@ begin
   s:= indeces.Item2;  
   repeat
     //Шаг 2
-    Result[k, s]:= min(self.storagesVector[k], self.shopsVector[s]);    
+    Result[k, s]:= min(self.storagesVector[k], self.shopsVector[s]);
+    self.flagContent[k, s]:= false;
     //Шаг 3
     self.storagesVector[k] -= Result[k,s];
     self.shopsVector[s] -= Result[k,s];
@@ -82,6 +84,60 @@ begin
         k:= getIndexOfMinInRow(self.rateMatrix, s, self.storagesVector, true);
     end;
   until (isMemberEmpty(self.storagesVector) = false) and (isMemberEmpty(self.shopsVector) = false);
+end;
+
+function TransportTaskSolver.calculateFunction(matrix: array[,] of real): real;
+begin
+  Result:= 0;
+  for var i := 0 to self.storagesVector.Length - 1 do
+    for var j := 0 to self.shopsVector.Length - 1 do
+      Result := Result + (self.rateMatrix[i,j] * matrix[i,j]);
+end;
+
+procedure TransportTaskSolver.findPotentials(storegesPotentials, shopsPotentials: array of real; matrix: array[,] of real);
+begin
+  var strorageCount := self.storagesVector.Length;
+  var shopsCount := self.shopsVector.Length;
+  fillVectorWithNaN(storegesPotentials);
+  fillVectorWithNaN(shopsPotentials);
+  
+  var storagesFlags := new boolean[strorageCount];
+  var shopsFlags := new boolean[shopsCount];  
+  fillVectorWithBool(storagesFlags, true);
+  fillVectorWithBool(shopsFlags, true);
+  
+  storegesPotentials[0] := 0;
+  shopsPotentials[0] := 0;
+  storagesFlags[0] := false;
+  while(true) do begin
+    for var i := 0 to strorageCount - 1 do
+      for var j := 0 to shopsCount - 1 do begin
+        if shopsFlags[j] and (matrix[i, j] > 0) and not real.IsNaN(storegesPotentials[i]) then begin
+          shopsPotentials[j] := self.rateMatrix[i,j] - storegesPotentials[i];
+          shopsFlags[j] := false;
+        end;
+      end;
+    for var j := 0 to shopsCount - 1 do
+      for var i := 0 to strorageCount - 1 do begin
+        if storagesFlags[i] and (matrix[i, j] > 0) and not real.IsNaN(shopsPotentials[j]) then begin
+          storegesPotentials[i] := self.rateMatrix[i,j] - shopsPotentials[j];
+          storagesFlags[i] := false;
+        end;
+      end;
+    if (indexOfNaN(shopsPotentials) = -1) and (indexOfNaN(storegesPotentials) = -1) then
+    break;
+  end;
+end;
+
+//Self func and procedure. End.
+
+function findIndecesOfMinRate(matrix: array[,] of real): (integer, integer);
+begin
+  Result:= (0,0);  
+  for var i:= 0 to matrix.GetLength(0) - 1 do
+    for var j:= 0 to matrix.GetLength(1) - 1 do
+      if(matrix[i,j] < matrix[Result[0], Result[1]]) then
+        Result:= (i, j);  
 end;
 
 function isMemberEmpty(member: array of real): Boolean;
@@ -116,12 +172,14 @@ begin
   end;         
 end;
 
-function TransportTaskSolver.calculateFunction(matrix: array[,] of real): real;
+function indexOfNaN(vector: array of real): integer;
 begin
-  Result:= 0;
-  for var i := 0 to self.storagesVector.Length - 1 do
-    for var j := 0 to self.shopsVector.Length - 1 do
-      Result := Result + (self.rateMatrix[i,j] * matrix[i,j]);
+  for var i:= 0 to vector.Length - 1 do
+    if(real.IsNaN(vector[i])) then begin      
+      result:= i;
+      exit;
+    end;
+    result:= -1;
 end;
 
 procedure printMatrix(matrix: array[,] of real);
@@ -131,6 +189,24 @@ begin
       write(matrix[i,j]:4);
     writeln();
   end;
-end;  
+end;
+
+procedure printVector(vector: array of real);
+begin
+  for var i:= 0 to vector.Length - 1 do
+    write(vector[i]);
+end;
+
+procedure fillVectorWithBool(vector: array of boolean; fillBy: boolean);
+begin
+  for var i:= 0 to vector.Length - 1 do
+    vector[i]:= fillBy;
+end;
+
+procedure fillVectorWithNaN(vector: array of real);
+begin
+  for var i:= 0 to vector.Length - 1 do
+    vector[i]:= real.NaN;
+end;
 
 end.
--- TransportTask.pas
+++ TransportTask.pas
@@ -5,17 +5,32 @@ var
     (2, 1, 3, 2),
     (2, 3, 3, 1),
     (3, 3, 2, 1)
-  );  
+  );
+  
+  flagContentExample := new boolean[3,4](
+    (true, true, true, true), 
+    (true, true, true, true), 
+    (true, true, true, true)
+  );
+  
   storagesVectorExample := new real[3](90, 70, 50);
   shopsVectorExample := new real[4](80, 60, 40, 30);
   
   transportTask: TransportTaskSolver := (
     rateMatrix: matrixExample;
+    flagContent: flagContentExample;
     storagesVector: storagesVectorExample;
     shopsVector: shopsVectorExample;
   );
-
+  
+  storagesPotentials := new real[3](0,0,0);
+  shopsPotentials := new real[4](0,0,0,0);
+  
 begin
   var tempMatrix:= transportTask.minTariffMethod();
   writeln(transportTask.calculateFunction(tempMatrix));
+  transportTask.findPotentials(storagesPotentials, shopsPotentials, tempMatrix);
+  printVector(storagesPotentials);
+  writeln();
+  printVector(shopsPotentials);
 end.
