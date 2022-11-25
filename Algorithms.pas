e17d38ead943930a0d289703e2b42f07b47fe585
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-11-25 09:48:08 +0300

    init: upload project

---
+++ Algorithms.pas
@@ -0,0 +1,80 @@
+﻿unit Algorithms;
+
+interface
+
+type TransportTaskSolver = record
+  rateMatrix: array[,] of real;
+  storagesVector: array of real;
+  shopsVector: array of real;
+  
+  //storageStrings: array of string;
+  //shopStrings: array of string;
+  
+  public constructor Create(
+    rateMatrix: array[,] of real;
+    storagesVector: array of real;
+    shopsVector: array of real
+  );
+  begin
+    self.rateMatrix := rateMatrix;
+    self.storagesVector := storagesVector;
+    self.shopsVector := shopsVector;
+    //storageStrings:= GetVectorStrings(storagesVector);
+    //shopStrings:= GetVectorStrings(shopsVector);
+  end;
+  
+  procedure Print;
+  //function GetVectorStrings(vectorToString: array of real): array of string;
+  
+end;
+
+
+function findIndecesOfMinRate(matrix: array[,] of real): (integer, integer);
+
+
+//function GetVectorStrings(vectorToString: array of real): array of string;
+
+//implement this
+//function minTariffMethod();
+
+implementation
+
+procedure TransportTaskSolver.Print;
+var charSpace: integer := 7;
+begin
+  Write('':charSpace, '│');
+  for var i := 0 to self.shopsVector.Length do
+    write('a' + i:charSpace );
+  Writeln();
+  Writeln('————————————————————————————————————————————————————');
+  {for var i := 0 to self.baseMatrix.GetLength(0) - 1 do
+  begin
+    Write(self.columnStringX[i]:charSpace, '│');
+    Write(self.baseMatrix[i, 0]:charSpace:2, '│');
+    for var j := 1 to Length(self.baseMatrix, 1) - 1 do
+      Write(self.baseMatrix[i, j]:charSpace:2);
+    Writeln();
+  end;}
+end;
+
+
+{function TransportTaskSolver.GetVectorStrings(vectorToString: array of real): array of string;
+begin
+  var len := vectorToString.Length;
+  Result := new string[len];
+  for var i := 0 to len do 
+    Result[i]:= char(96+i);
+end;
+}
+
+function findIndecesOfMinRate(matrix: array[,] of real): (integer, integer);
+begin
+  Result:= (0,0);
+  
+  for var i:= 0 to matrix.GetLength(0) - 1 do
+    for var j:= 0 to matrix.GetLength(1) - 1 do
+      if(matrix[i,j] < matrix[Result[0], Result[1]]) then
+        Result:= (i, j);
+  
+end;  
+end.
---
+++ TransportTask.pas
@@ -0,0 +1,21 @@
+﻿uses Algorithms;
+
+var
+  matrixExample := new real[3,4](
+    (2, 1, 3, 2),
+    (2, 3, 3, 1),
+    (3, 3, 2, 1)
+  );  
+  storagesVectorExample := new real[3](90, 70, 50);
+  shopsVectorExample := new real[4](80, 60, 40, 30);
+  
+  transportTask: TransportTaskSolver := (
+    rateMatrix: matrixExample;
+    storagesVector: storagesVectorExample;
+    shopsVector: shopsVectorExample;
+  );
+
+begin
+  transportTask.Print();
+  writeln(findIndecesOfMinRate(matrixExample));
+end.
