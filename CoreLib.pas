32e137810b4cfa5b413081a2c70fbf3cc900e36d
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-12-18 15:06:06 +0300

    feat: use lib with classes

---
+++ CoreLib.pas
@@ -0,0 +1,421 @@
+﻿unit CoreLib;
+
+interface
+
+type TransportationMatrix = class
+  private
+    _cargoToTransitMatrix: array[,] of integer;
+    _rateMatrix: array[,] of integer;
+    _manufacturerVolumes: array of integer;
+    _customerVolumes: array of integer;
+    _potentialsRow: array of integer;
+    _potentialsColumn: array of integer;
+    
+    function FindPathStep(pivotIndexes: (integer, integer); visited: List<(integer, integer)>): boolean;
+  public
+    constructor(
+      rateMatrix: array[,] of integer;
+      manufacturerVolumes: array of integer;
+      customerVolumes: array of integer;
+      potentialsRow: array of integer;
+      potentialsColumn: array of integer
+    );
+    begin
+      _rateMatrix := rateMatrix;
+      _cargoToTransitMatrix := new integer[rateMatrix.GetLength(0), rateMatrix.GetLength(1)];
+      _manufacturerVolumes := manufacturerVolumes;
+      _customerVolumes := customerVolumes;
+      _potentialsRow := potentialsRow;
+      _potentialsColumn := potentialsColumn;
+    end;
+    
+    property RateMatrix: array[,] of integer read _rateMatrix;
+    property ManufacturerVolumes: array of integer read _manufacturerVolumes;
+    property CustomerVolumes: array of integer read _customerVolumes;
+    
+    procedure Print();
+    procedure DistributeCargo();
+    procedure CalculatePotentials();
+    procedure Optimize(path: array of (integer, integer));
+    function FindMinRateCellIndexes(): (integer, integer);
+    function IsDistributeComplete(): boolean;
+    function FindPivotCellIndexes(): (integer, integer);
+    function FindPath(pivotIndexes: (integer, integer)): array of (integer, integer);
+    function CalculatePrice(): integer;
+end;
+
+implementation
+
+procedure TransportationMatrix.Print();
+var
+  cellWidth: integer := 9;
+begin
+  // potentials row
+  Write('':cellWidth + 6);
+  for var i := 0 to self._potentialsRow.Length - 1 do
+    Write($'{self._potentialsRow[i]}    ':cellWidth + 1);
+  Writeln();
+  
+  // 1st row
+  Write('':cellWidth + 5, '|');
+  for var i := 0 to self._rateMatrix.GetLength(1) - 1 do
+    Write('':cellWidth, '|');
+  Writeln();
+  Write('':cellWidth + 5, '|');
+  for var i := 0 to self._rateMatrix.GetLength(1) - 1 do
+    Write($'a{i + 1}   ':cellWidth, '|');
+  Writeln();
+  Write('':cellWidth + 5, '|');
+  for var i := 0 to self._rateMatrix.GetLength(1) - 1 do
+    Write('':cellWidth, '|');
+  Writeln();
+  
+  // divider
+  Write('——————————':cellWidth + 6);
+  for var i := 0 to self._rateMatrix.GetLength(1) do
+    Write('——————————':cellWidth + 1);
+  Writeln();
+  
+  // matrix
+  for var i := 0 to self._rateMatrix.GetLength(0) - 1 do
+  begin
+    // rates row
+    Write('':cellWidth + 5, '|');
+    for var j := 0 to self._rateMatrix.GetLength(1) - 1 do
+      Write(self._rateMatrix[i,j]:cellWidth, '|');
+    Writeln();
+    // empty row
+    Write('':cellWidth + 5, '|');
+    for var j := 0 to self._rateMatrix.GetLength(1) - 1 do
+      Write('':cellWidth, '|');
+    Writeln();
+    
+    // potential column
+    Write($'{self._potentialsColumn[i]}':cellWidth - 4);
+    
+    // cargo to transit row
+    Write($'A{i + 1}   ':cellWidth, '|');
+    for var j := 0 to self._cargoToTransitMatrix.GetLength(1) - 1 do
+      Write($'{self._cargoToTransitMatrix[i,j]}   ':cellWidth, '|');
+    // manufacturer volume
+    Write($'{self._manufacturerVolumes[i]}   ':cellWidth);
+    Writeln();
+    // empty row
+    Write('':cellWidth + 5, '|');
+    for var j := 0 to self._rateMatrix.GetLength(1) - 1 do
+      Write('':cellWidth, '|');
+    Writeln();
+    Write('':cellWidth + 5, '|');
+    for var j := 0 to self._rateMatrix.GetLength(1) - 1 do
+      Write('':cellWidth, '|');
+    Writeln();
+    // divider
+    Write('——————————':cellWidth + 6);
+    for var j := 0 to self._rateMatrix.GetLength(1) do
+      Write('——————————':cellWidth + 1);
+    Writeln();
+  end;
+  
+  // customer volume
+  Write('':cellWidth + 5, '|');
+  for var i := 0 to self._rateMatrix.GetLength(1) - 1 do
+      Write('':cellWidth, '|');
+  Writeln();
+  Write('':cellWidth + 5, '|');
+  for var i := 0 to self._rateMatrix.GetLength(1) - 1 do
+    Write($'{self._customerVolumes[i]}   ':cellWidth, '|');
+  Writeln();
+  Write('':cellWidth + 5, '|');
+  for var i := 0 to self._rateMatrix.GetLength(1) - 1 do
+    Write('':cellWidth, '|');
+  Writeln();
+end;
+
+procedure TransportationMatrix.DistributeCargo();
+var
+  currentIndexes: (integer, integer);
+  manufacturerVolumes: array of integer := new integer[self._manufacturerVolumes.Length];
+  customerVolumes: array of integer := new integer[self._customerVolumes.Length];
+begin
+  self._manufacturerVolumes.CopyTo(manufacturerVolumes, 0);
+  self._customerVolumes.CopyTo(customerVolumes, 0);
+  currentIndexes := self.FindMinRateCellIndexes();
+  
+  while (manufacturerVolumes.Any(v -> v <> 0) and customerVolumes.Any(v -> v <> 0)) do
+  begin
+    var currentManufacturerVolume := manufacturerVolumes[currentIndexes.Item1];
+    var currentCustomerVolume := customerVolumes[currentIndexes.Item2];
+    
+    if (currentManufacturerVolume > currentCustomerVolume) then
+    begin // customer's demand is satisfied
+      manufacturerVolumes[currentIndexes.Item1] -= currentCustomerVolume;
+      customerVolumes[currentIndexes.Item2] := 0;
+      self._cargoToTransitMatrix[currentIndexes.Item1, currentIndexes.Item2] :=
+        currentCustomerVolume;
+      
+      // find new index in row
+      var minRate := MaxInt;
+      for var i := 0 to self._cargoToTransitMatrix.GetLength(1) - 1 do
+      begin
+        if ((i = currentIndexes.Item2) or (customerVolumes[i] = 0)) then
+          continue;
+        
+        var currentRowRate := self._rateMatrix[currentIndexes.Item1, i];
+        if (minRate > currentRowRate) then
+        begin
+          minRate := currentRowRate;
+          currentIndexes := (currentIndexes.Item1, i);
+        end;
+      end;
+    end
+    else if (currentManufacturerVolume < currentCustomerVolume) then
+    begin // the manufacturer's volume is exhausted
+      manufacturerVolumes[currentIndexes.Item1] := 0;
+      customerVolumes[currentIndexes.Item2] -= currentManufacturerVolume;
+      self._cargoToTransitMatrix[currentIndexes.Item1, currentIndexes.Item2] :=
+        currentManufacturerVolume;
+      
+      // find new index in column
+      var minRate := MaxInt;
+      for var i := 0 to self._cargoToTransitMatrix.GetLength(0) - 1 do
+      begin
+        if ((i = currentIndexes.Item1) or (manufacturerVolumes[i] = 0)) then
+          continue;
+        
+        var currentColumnRate := self._rateMatrix[i, currentIndexes.Item2];
+        if (minRate > currentColumnRate) then
+        begin
+          minRate := currentColumnRate;
+          currentIndexes := (i, currentIndexes.Item2);
+        end;
+      end;
+    end
+    else
+    begin // the volumes of the manufacturer and the customer are equal
+      manufacturerVolumes[currentIndexes.Item1] := 0;
+      CustomerVolumes[currentIndexes.Item2] := 0;
+      self._cargoToTransitMatrix[currentIndexes.Item1, currentIndexes.Item2] :=
+        currentCustomerVolume;
+      
+      var minRate := MaxInt;
+      // find new indexes in whole matrix
+      for var i := 0 to self._rateMatrix.GetLength(0) - 1 do
+      begin
+        if ((i = currentIndexes.Item1) or (manufacturerVolumes[i] = 0)) then
+          continue;
+        
+        for var j := 0 to self._rateMatrix.GetLength(1) - 1 do
+        begin
+          if ((j = currentIndexes.Item2) or (customerVolumes[j] = 0)) then
+            continue;
+          
+          if (minRate > self._rateMatrix[i,j]) then
+          begin
+            minRate := self._rateMatrix[i,j];
+            currentIndexes := (i, j);
+          end;
+        end;
+      end;
+    end;
+  end;
+end;
+
+procedure TransportationMatrix.CalculatePotentials();
+var
+  isCalculatedRow: array of boolean := ArrFill(self._potentialsRow.Length, false);
+  isCalculatedColumn: array of boolean := ArrFill(self._potentialsColumn.Length, false);
+begin
+  isCalculatedColumn[0] := true;
+  self._potentialsColumn[0] := 0;
+  
+  while (isCalculatedRow.Any(x -> not x) or isCalculatedColumn.Any(x -> not x)) do
+  begin
+    for var i := 0 to self._rateMatrix.GetLength(0) - 1 do
+      for var j := 0 to self._rateMatrix.GetLength(1) - 1 do
+      begin
+        if (
+          (isCalculatedRow[j] and isCalculatedColumn[i])
+          or (self._cargoToTransitMatrix[i,j] = 0)
+        ) then
+          continue;
+        
+        if (isCalculatedColumn[i] and not isCalculatedRow[j]) then
+        begin
+          self._potentialsRow[j] := self._rateMatrix[i,j] - self._potentialsColumn[i];
+          isCalculatedRow[j] := true;
+        end
+        else if (isCalculatedRow[j] and not isCalculatedColumn[i]) then
+        begin
+          self._potentialsColumn[i] := self._rateMatrix[i,j] - self._potentialsRow[j];
+          isCalculatedColumn[i] := true;
+        end;
+      end;
+  end;
+end;
+
+procedure TransportationMatrix.Optimize(path: array of (integer, integer));
+var
+  minCargoValue: integer := MaxInt;
+begin
+  // find min cargo value
+  for var i := 0 to path.Length - 1 do
+  begin
+    var currentCargoValue := self._cargoToTransitMatrix[path[i].Item1, path[i].Item2];
+    if ((i mod 2 = 1) and (minCargoValue > currentCargoValue)) then
+      minCargoValue := currentCargoValue;
+  end;
+  
+  // optimize
+  for var i := 0 to path.Length - 1 do
+    if (i mod 2 = 0) then
+      self._cargoToTransitMatrix[path[i].Item1, path[i].Item2] += minCargoValue
+    else
+      self._cargoToTransitMatrix[path[i].Item1, path[i].Item2] -= minCargoValue;
+end;
+
+function TransportationMatrix.FindMinRateCellIndexes: (integer, integer);
+var
+  minRate: integer := MaxInt;
+begin
+  for var i := 0 to self._rateMatrix.GetLength(0) -1  do
+    for var j := 0 to self._rateMatrix.GetLength(1) - 1 do
+      if (minRate > self._rateMatrix[i,j]) then
+      begin
+        minRate := self._rateMatrix[i,j];
+        Result := (i, j);
+      end;
+end;
+
+function TransportationMatrix.IsDistributeComplete(): boolean;
+var
+  rowSum: integer := 0;
+  columnSum: integer := 0;
+begin
+  Result := true;
+  // comparison with the manufacturer's volumes
+  for var i := 0 to self._cargoToTransitMatrix.GetLength(0) - 1 do
+  begin
+    rowSum := 0;
+    for var j := 0 to self._cargoToTransitMatrix.GetLength(1) - 1 do
+      rowSum += self._cargoToTransitMatrix[i, j];
+    
+    if (rowSum <> self._manufacturerVolumes[i]) then
+    begin
+      Result := false;
+      break;
+    end;
+  end;
+  
+  // comparison with the manufacturer's volumes
+  for var i := 0 to self._cargoToTransitMatrix.GetLength(1) - 1 do
+  begin
+    columnSum := 0;
+    for var j := 0 to self._cargoToTransitMatrix.GetLength(0) - 1 do
+      columnSum += self._cargoToTransitMatrix[j, i];
+    
+    if (columnSum <> self._customerVolumes[i]) then
+    begin
+      Result := false;
+      break;
+    end;
+  end;
+end;
+
+function TransportationMatrix.FindPivotCellIndexes(): (integer, integer);
+var
+  minDifference: integer := MaxInt;
+  difference: integer;
+begin
+  Result := (-1, -1);
+  for var i := 0 to self._rateMatrix.GetLength(0) - 1 do
+    for var j := 0 to self._rateMatrix.GetLength(1) - 1 do
+    begin
+      difference := self._rateMatrix[i,j] - self._potentialsRow[j] - self._potentialsColumn[i];
+      if ((difference < 0) and (minDifference > difference)) then
+      begin
+        minDifference := difference;
+        Result := (i, j);
+      end;
+    end;
+end;
+
+function TransportationMatrix.FindPath(pivotIndexes: (integer, integer)): array of (integer, integer);
+var
+  path: List<(integer, integer)> := new List<(integer, integer)>;
+  
+begin
+  path.Add(pivotIndexes);
+  self.FindPathStep(pivotIndexes, path);
+  
+  Result := path.ToArray();
+end;
+
+function TransportationMatrix.FindPathStep(
+  pivotIndexes: (integer, integer);
+  visited: List<(integer, integer)>
+): boolean;
+var
+  nextCells: Queue<(integer, integer)> := new Queue<(integer, integer)>;
+begin
+  Result := false;
+  
+  // if element finded
+  if (visited.Count > 3) then
+  begin
+    var originPivotElement := visited.First();
+    if (
+      (visited.Count mod 2 = 0)
+      and ((pivotIndexes.Item1 = originPivotelement.Item1) or (pivotIndexes.Item2 = originPivotElement.Item2))
+    ) then
+      begin
+        Result := true;
+        exit;          
+      end;
+  end;
+  
+  // find next cells in column
+  for var i := 0 to self._cargoToTransitMatrix.GetLength(0) - 1 do
+  begin
+    if (
+      (i = pivotIndexes.Item1)
+      or visited.Contains((i, pivotIndexes.Item2))
+      or (self._cargoToTransitMatrix[i, pivotIndexes.Item2] = 0)
+    ) then
+      continue;
+    
+    nextCells.Enqueue((i, pivotIndexes.Item2));
+  end;
+  
+  // find next cells in row
+  for var i := 0 to self._cargoToTransitMatrix.GetLength(1) - 1 do
+  begin
+    if (
+      (i = pivotIndexes.Item2)
+      or visited.Contains((pivotIndexes.Item1, i))
+      or (self._cargoToTransitMatrix[pivotIndexes.Item1, i] = 0)
+    ) then
+      continue;
+    
+    nextCells.Enqueue((pivotIndexes.Item1, i));
+  end;
+  
+  while (nextCells.Count > 0) do
+  begin
+    var nextCell := nextCells.Dequeue();
+    visited.Add(nextCell);
+    Result := self.FindPathStep(nextCell, visited);
+    if (Result = true) then
+      exit;
+    visited.RemoveAt(visited.Count - 1);
+  end;
+end;
+
+function TransportationMatrix.CalculatePrice(): integer;
+begin
+  Result := 0;
+  for var i := 0 to self._cargoToTransitMatrix.GetLength(0) - 1 do
+    for var j := 0 to self._cargoToTransitMatrix.GetLength(1) - 1 do
+      Result += self._cargoToTransitMatrix[i, j] * self._rateMatrix[i, j];
+end;
+
+end.
--- TransportTask.pas
+++ TransportTask.pas
@@ -1,90 +1,155 @@
-﻿uses Algorithms;
+﻿uses CoreLib;
+
 var
+  optimizingStep: integer := 1;
+  currentExample: TransportationMatrix;
   
-  //First Example. Start
-  {matrixExample := new real[3,4](
+  rateMatrixExample1: array[,] of integer := (
+    ( 7, 1, 4,  5,  2),
+    (13, 4, 7,  6,  3),
+    ( 3, 8, 0, 18, 12),
+    ( 9, 5, 3,  4,  7)
+  );
+  manufacturerVolumesExample1: array of integer := (85, 112, 72, 120);
+  customerVolumesExample1: array of integer := (75, 125, 64, 65, 60);
+  potentialsRowExample1: array of integer := ArrFill(5, 0);
+  potentialsColumnExample1: array of integer := ArrFill(4, 0);
+  example1: TransportationMatrix := new TransportationMatrix(
+    rateMatrixExample1,
+    manufacturerVolumesExample1,
+    customerVolumesExample1,
+    potentialsRowExample1,
+    potentialsColumnExample1
+  );
+  
+  rateMatrixExample2: array[,] of integer := (
     (2, 1, 3, 2),
     (2, 3, 3, 1),
     (3, 3, 2, 1)
-  );  
-  flagContentExample := new boolean[3,4](
-    (true, true, true, true), 
-    (true, true, true, true), 
-    (true, true, true, true)
-  );  
-  storagesVectorExample := new real[3](90, 70, 50);
-  shopsVectorExample := new real[4](80, 60, 40, 30);
-  
-  storagesPotentials := new real[3](0,0,0);
-  shopsPotentials := new real[4](0,0,0,0);
-  //First Example. End}
-  
-  //Second Example. Start
-  matrixExample := new real[4,5](
-    (7, 1, 4, 5, 2),
-    (13, 4, 7, 6, 3),
-    (3, 8, 0, 18, 12),
-    (9, 5, 3, 4, 7)
-  );  
-  flagContentExample := new boolean[4,5](
-    (true, true, true, true, true), 
-    (true, true, true, true, true),
-    (true, true, true, true, true),
-    (true, true, true, true, true)
-  );  
-  storagesVectorExample := new real[4](85, 112, 72, 120);
-  shopsVectorExample := new real[5](75, 125, 64, 65, 60);
-  
-  storagesPotentials := new real[4](0,0,0,0);
-  shopsPotentials := new real[5](0,0,0,0,0);
-  //Second Example. End
-  
-  transportTask: TransportTaskSolver := (
-    rateMatrix: matrixExample;
-    flagContent: flagContentExample;
-    storagesVector: storagesVectorExample;
-    shopsVector: shopsVectorExample;
+  );
+  manufacturerVolumesExample2: array of integer := (90, 70, 50);
+  customerVolumesExample2: array of integer := (80, 60, 40, 30);
+  potentialsRowExample2: array of integer := new integer[4];
+  potentialsColumnExample2: array of integer := new integer[3];
+  example2: TransportationMatrix := new TransportationMatrix(
+    rateMatrixExample2,
+    manufacturerVolumesExample2,
+    customerVolumesExample2,
+    potentialsRowExample2,
+    potentialsColumnExample2
   );
   
-  startIndeces: (integer, integer);
+  rateMatrixExample3: array[,] of integer := (
+    (12,  5,  3, 11),
+    ( 2, 17, 30,  4),
+    (23,  3, 12,  1)
+  );
+  manufacturerVolumesExample3: array of integer := (55, 93, 27);
+  customerVolumesExample3: array of integer := (42, 63, 57, 13);
+  potentialsRowExample3: array of integer := new integer[4];
+  potentialsColumnExample3: array of integer := new integer[3];
+  example3: TransportationMatrix := new TransportationMatrix(
+    rateMatrixExample3,
+    manufacturerVolumesExample3,
+    customerVolumesExample3,
+    potentialsRowExample3,
+    potentialsColumnExample3
+  );
   
+  rateMatrixExample4: array[,] of integer := (
+    ( 2, 24,  4,  2,  3),
+    (20, 10, 15, 27,  7),
+    (15, 15, 12, 25, 19),
+    ( 2,  6,  3,  5,  5)
+  );
+  manufacturerVolumesExample4: array of integer := (28, 13, 15, 30);
+  customerVolumesExample4: array of integer := (27, 16, 25, 11, 7);
+  potentialsRowExample4: array of integer := new integer[5];
+  potentialsColumnExample4: array of integer := new integer[4];
+  example4: TransportationMatrix := new TransportationMatrix(
+    rateMatrixExample4,
+    manufacturerVolumesExample4,
+    customerVolumesExample4,
+    potentialsRowExample4,
+    potentialsColumnExample4
+  );
+  
+  rateMatrixExample5: array[,] of integer := (
+    (16, 30, 17, 10,  4),
+    (30, 27, 26,  9, 23),
+    (13,  4, 22,  3,  1),
+    ( 3,  1,  5,  4, 24)
+  );
+  manufacturerVolumesExample5: array of integer := (4, 6, 10, 10);
+  customerVolumesExample5: array of integer := (7, 7, 7, 7, 2);
+  potentialsRowExample5: array of integer := new integer[5];
+  potentialsColumnExample5: array of integer := new integer[4];
+  example5: TransportationMatrix := new TransportationMatrix(
+    rateMatrixExample5,
+    manufacturerVolumesExample5,
+    customerVolumesExample5,
+    potentialsRowExample5,
+    potentialsColumnExample5
+  );
+  
+  rateMatrixExample6: array[,] of integer := (
+    ( 5, 15,  3,  6, 10),
+    (23,  8, 13, 27, 12),
+    (30,  1,  5, 24, 25),
+    ( 8, 26,  7, 28,  9)
+  );
+  manufacturerVolumesExample6: array of integer := (9, 11, 14, 16);
+  customerVolumesExample6: array of integer := (8, 9, 13, 8, 12);
+  potentialsRowExample6: array of integer := new integer[5];
+  potentialsColumnExample6: array of integer := new integer[4];
+  example6: TransportationMatrix := new TransportationMatrix(
+    rateMatrixExample6,
+    manufacturerVolumesExample6,
+    customerVolumesExample6,
+    potentialsRowExample6,
+    potentialsColumnExample6
+  );
+
 begin
-  // составление плана
-  var matrix := new real[transportTask.rateMatrix.GetLength(0), transportTask.rateMatrix.GetLength(1)];
-  //startIndeces := findIndecesOfMinRate(transportTask.rateMatrix);
-  startIndeces := (2, 2);
-  writeln('---------------- Составление плана ----------------');
-  while(true) do begin
-    transportTask.step(startIndeces, matrix);
-    //printMatrix(matrix); 
-    var prevIndeces:= startIndeces;
-    startIndeces := transportTask.findMinRate(prevIndeces);
-    if(startIndeces = (-1,-1)) then
-      break;
+//  Console.OutputEncoding := System.Text.Encoding.GetEncoding(866);
+//  TextColor(15); // белый цвет шрифта в консоли
+  Writeln('Исходная матрица');
+  
+  currentExample := example6;
+  currentExample.Print();
+  
+  var minRateIndexes := currentExample.FindMinRateCellIndexes();
+  Writeln(
+    'Индекс элемента с минимальным тарифом: ',
+    $'({minRateIndexes.Item1 + 1} {minRateIndexes.Item2 + 1})'
+  );
+  
+  Writeln('==============================================================================');
+  currentExample.DistributeCargo();
+  currentExample.CalculatePotentials();
+  Writeln('Результат первоначального распределения');
+  currentExample.Print();
+  
+  while (true) do
+  begin
+    var pivotCellIndexes := currentExample.FindPivotCellIndexes();
+    if (pivotCellIndexes = (-1, -1)) then
+    begin
+      Writeln('Ответ: ');
+      var totalPrice := currentExample.CalculatePrice();
+      Writeln($'f = {totalPrice}');
+      exit;
+    end;
+    
+    var path := currentExample.FindPath(pivotCellIndexes);
+    Writeln($'Индекс начального элемента контура: ({path[0].Item1 + 1} {path[0].Item2 + 1})');
+    
+    Writeln('============================================================================');
+    currentExample.Optimize(path);
+    currentExample.CalculatePotentials();
+    Writeln($'Оптимизация {optimizingStep}');
+    currentExample.Print();
+    
+    optimizingStep += 1;    
   end;
-  //var matrix:= transportTask.minTariffMethod();
-  printMatrix(matrix);  
-  
-  // оптимизация плана
-  writeln('---------------- Оптимизация плана ----------------');
-  while (true) do begin
-    writeln();
-    writeln('F = ', transportTask.calculateFunction(matrix));
-  transportTask.findPotentials(storagesPotentials, shopsPotentials, matrix);
-  writeln();
-  var indeces := transportTask.checkPotentials(storagesPotentials, shopsPotentials, matrix);
-  if(indeces.Item1 = -1) and (indeces.Item2 = -1) then
-    break
-  else begin
-      transportTask.findContour(indeces, matrix);
-    end;    
-  end;
-  
-  writeln('---------------- Ответ ----------------');
-  writeln();
-  writeln('---------------- Конечный план ----------------');
-  printMatrix(matrix);
-  writeln();
-  writeln('F = ', transportTask.calculateFunction(matrix));
-  
 end.
